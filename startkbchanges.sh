4740a5d8e54ed492d3308bda1ec661b846265fd6
shrinidhihr98 <shrinidhihr98@gmail.com>
2016-10-15 21:46:54 +0530

    Update startkbchanges.sh

--- startkbchanges.sh
+++ startkbchanges.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# This line calls xmodmap to use ~/.Xmodmap
 xmodmap /home/user/.Xmodmap
+
+#This calls xcape and refreshes it, and gives some arguments.
 /home/user/xcape/xcape
 killall /home/user/xcape/xcape
 /home/user/xcape/xcape -e 'Control_L=Escape;Shift_L=BackSpace;Shift_R=BackSpace' -t 300
