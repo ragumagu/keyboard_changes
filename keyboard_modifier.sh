789ad8e6d9473e982e431a9ec3840883419a7842
shrinidhihr98 <shrinidhihr98@gmail.com>
2016-11-01 13:45:40 +0530

    Update and rename startkbchanges.sh to keyboard_modifier.sh

---
+++ keyboard_modifier.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This file puts the keyboard layout in modifier mode. That is, the space bar multiplexes with control and shift with backspace
+#and caps lock is turned into control and escape.
+# This line calls xmodmap to use ~/.xmodmap
+xmodmap /home/user/.xmodmap
+
+#This calls xcape and refreshes it, and gives some arguments.
+/home/user/xcape/xcape
+killall /home/user/xcape/xcape
+/home/user/xcape/xcape -e 'Control_L=Escape;Shift_L=BackSpace;Shift_R=BackSpace;Hyper_L=space' -t 200
