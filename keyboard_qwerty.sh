ac5eed0e2774124d0a7941595f437900210cfbaa
shrinidhihr98 <shrinidhihr98@gmail.com>
2016-12-01 19:32:48 +0530

    Update keyboard_qwerty.sh

--- keyboard_qwerty.sh
+++ keyboard_qwerty.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #This file puts the keyboard layout in qwerty mode. That is, although xcape allows shift with backspace, spacebar remains as
 #a space and caps lock is turned into a escape.
-# This line calls xmodmap to use ~/.xmodmap_qwerty
-xmodmap /home/user/.xmodmap_qwerty
+# This line calls xmodmap to use ~/.xmodmap_space_control_off
+xmodmap /home/user/.xmodmap_space_control_off
 
 #This calls xcape and refreshes it, and gives some arguments.
 /home/user/xcape/xcape
 killall /home/user/xcape/xcape
 /home/user/xcape/xcape -e 'Shift_L=BackSpace;Shift_R=BackSpace' -t 200
+echo "Keyboard layout is now in space_control_off mode."
