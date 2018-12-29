b4e9865e61f0d8aa3b45b66dc3ac1ce3ede5f34f
shrinidhihr98 <shrinidhihr98@gmail.com>
2018-12-29 20:07:45 +0530

    Update keyboard_space_control_off.sh

--- keyboard_space_control_off.sh
+++ keyboard_space_control_off.sh
@@ -5,7 +5,7 @@
 xmodmap /home/user/keyboard_changes/xmodmap_space_control_off
 
 #This calls xcape and refreshes it, and gives some arguments.
-/home/user/xcape/xcape
-killall /home/user/xcape/xcape
-/home/user/xcape/xcape -e 'Shift_L=BackSpace;Shift_R=BackSpace' -t 200
+xcape
+killall xcape
+xcape -e 'Shift_L=BackSpace;Shift_R=BackSpace' -t 100
 echo "Keyboard layout is now in space_control_off mode."
