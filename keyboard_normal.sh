a675ad93c12f75101fcb8b60d3d24039d215fa9a
shrinidhihr98 <shrinidhihr98@gmail.com>
2018-12-30 11:16:25 +0530

    Update keyboard_normal.sh

--- keyboard_normal.sh
+++ keyboard_normal.sh
@@ -2,7 +2,7 @@
 #This file puts the keyboard layout in modifier mode. That is, the space bar multiplexes with control and shift with backspace
 #and caps lock is turned into control and escape.
 # This line calls xmodmap to use ~/keyboard_changes/xmodmap_normal
-xmodmap /home/user/keyboard_changes/xmodmap_normal
+xkbcomp /home/user/keyboard_changes/xkbNormal $DISPLAY
 
 #This calls xcape and refreshes it, and gives some arguments.
 xcape
