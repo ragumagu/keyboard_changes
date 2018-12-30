ce86fbd349f6e86e3a9d2cd42b51b8b9de5135ca
shrinidhihr98 <shrinidhihr98@gmail.com>
2018-12-30 11:44:12 +0530

    Update keyboard_space_control_off.sh

--- keyboard_space_control_off.sh
+++ keyboard_space_control_off.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#This file turns off the dual role of Spacebar as Space and Control. After executing this, the spacebar is no longer a modifier, and only emits a space character. However, the other changes remain.
+# This file turns off the dual role of Spacebar as Space and Control. After executing this, the spacebar is no longer a modifier, 
+# and only emits a space character. However, the other changes remain.
 
 # This line calls xkbcomp to use ~/keyboard_changes/xkbSCO layout (xkbSpaceControlOff).
 xkbcomp /home/shrinidhi/keyboard_changes/xkbSCO $DISPLAY
