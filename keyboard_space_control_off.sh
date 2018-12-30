2125a6208f0c4ecc45a6a37deb2f1bcfb78351e3
shrinidhihr98 <shrinidhihr98@gmail.com>
2018-12-30 11:19:24 +0530

    Update keyboard_space_control_off.sh

--- keyboard_space_control_off.sh
+++ keyboard_space_control_off.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This file puts the keyboard layout in qwerty mode. That is, although xcape allows shift with backspace, spacebar remains as
 #a space and caps lock is turned into a escape.
-# This line calls xmodmap to use ~/.xmodmap_space_control_off
+# This line calls xkbcomp to use ~/keyboard_changes/xkbSCO layout (xkbSpaceControlOff).
 xkbcomp /home/shrinidhi/keyboard_changes/xkbSCO $DISPLAY
 
 #This calls xcape and refreshes it, and gives some arguments.
